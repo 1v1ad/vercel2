8ba12d05b7b8ca3b01112ac588efa39c660c6c01
1v1ad <1v1ad@proton.me>
2025-07-30 10:36:28 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,48 +1,54 @@
-import Script from "next/script";
-import { useEffect } from "react";
+// pages/index.js
+
+import { useEffect } from 'react';
 
 export default function Home() {
   useEffect(() => {
-    if (window.VKIDSDK) {
-      const VKID = window.VKIDSDK;
+    const script = document.createElement('script');
+    script.src = 'https://unpkg.com/@vkid/sdk@2.7.3/dist-sdk/umd/index.js';
+    script.onload = () => {
+      if ('VKIDSDK' in window) {
+        const VKID = window.VKIDSDK;
 
-      VKID.Config.init({
-        app: 53969710,
-        redirectUrl: 'https://vercel2-eight-blue.vercel.app/api/vk/callback',
-        responseMode: VKID.ConfigResponseMode.Callback,
-        source: VKID.ConfigSource.LOWCODE,
-        scope: '',
-      });
-
-      const oneTap = new VKID.OneTap();
-
-      oneTap
-        .render({
-          container: document.getElementById("vk-container"),
-          showAlternativeLogin: true,
-          oauthList: ["mail_ru"],
-        })
-        .on(VKID.WidgetEvents.ERROR, (error) => {
-          console.error("VK ERROR", error);
-        })
-        .on(VKID.OneTapInternalEvents.LOGIN_SUCCESS, (payload) => {
-          const { code, device_id } = payload;
-
-          VKID.Auth.exchangeCode(code, device_id)
-            .then((res) => console.log("VKID Auth success", res))
-            .catch((err) => console.error("VKID Auth failed", err));
+        VKID.Config.init({
+          app: 53969710,
+          redirectUrl: 'https://vercel2-eight-blue.vercel.app/api/vk/callback',
+          responseMode: VKID.ConfigResponseMode.Callback,
+          source: VKID.ConfigSource.LOWCODE,
+          scope: '',
         });
-    }
+
+        const oneTap = new VKID.OneTap();
+
+        oneTap
+          .render({
+            container: document.getElementById('vk_button'),
+            showAlternativeLogin: true,
+            oauthList: ['mail_ru'],
+          })
+          .on(VKID.WidgetEvents.ERROR, (err) => console.error('VKID error', err))
+          .on(VKID.OneTapInternalEvents.LOGIN_SUCCESS, function (payload) {
+            const code = payload.code;
+            const deviceId = payload.device_id;
+
+            VKID.Auth.exchangeCode(code, deviceId)
+              .then((res) => {
+                console.log('VKID auth success', res);
+                alert('Успешно вошли! Смотри консоль');
+              })
+              .catch((err) => {
+                console.error('VKID exchange error', err);
+              });
+          });
+      }
+    };
+    document.body.appendChild(script);
   }, []);
 
   return (
-    <>
-      <Script
-        src="https://unpkg.com/@vkid/sdk@3.0.0/dist-sdk/umd/index.js"
-        strategy="beforeInteractive"
-      />
+    <div>
       <h1>VK One Tap Login</h1>
-      <div id="vk-container"></div>
-    </>
+      <div id="vk_button"></div>
+    </div>
   );
 }
