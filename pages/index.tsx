e9827a65a35204c0c111b1cd76c8bdf04ef875c1
1v1ad <1v1ad@proton.me>
2025-07-30 09:33:46 +0300

    Add files via upload

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,22 @@
-import Script from "next/script"
+import Script from "next/script";
 
 export default function Home() {
   return (
     <div>
       <h1>VK One Tap Login</h1>
-      <div id="vk_ontap_auth"></div>
+      <div id="vk_ontap_auth" />
       <Script
-        src="https://vk.com/js/api/openapi.js?169"
         strategy="afterInteractive"
+        src="https://vk.com/js/api/openapi.js?169"
         onLoad={() => {
           // @ts-ignore
           if (window.VK) {
             // @ts-ignore
-            window.VK.init({ apiId: parseInt(process.env.NEXT_PUBLIC_VK_APP_ID || "0") });
-            // @ts-ignore
-            window.VK.Widgets.Auth("vk_ontap_auth", { width: "280px" });
+            window.VK.Widgets.Auth("vk_ontap_auth", {
+              onAuth: function(userData) {
+                alert("Добро пожаловать, " + userData.first_name);
+              }
+            });
           }
         }}
       />
