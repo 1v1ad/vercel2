8136ea2f5270dff26a9b2d697c888ca5fb92e99d
1v1ad <1v1ad@proton.me>
2025-07-30 10:25:08 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,37 +1,43 @@
+import { useEffect } from 'react';
+
 export default function Home() {
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.src = 'https://unpkg.com/@vkid/sdk@3.0.0/dist-sdk/umd/index.js';
+    script.async = true;
+    script.onload = () => {
+      if ('VKIDSDK' in window) {
+        const VKID = window.VKIDSDK;
+
+        VKID.Config.init({
+          app: 53969710,
+          redirectUrl: 'https://vercel2-eight-blue.vercel.app/api/vk/callback',
+          responseMode: VKID.ConfigResponseMode.Callback,
+          source: VKID.ConfigSource.LOWCODE,
+        });
+
+        const oneTap = new VKID.OneTap();
+        oneTap.render({
+          container: document.getElementById('vk-button'),
+          showAlternativeLogin: true,
+          oauthList: ['mail_ru'],
+        })
+        .on(VKID.WidgetEvents.ERROR, console.error)
+        .on(VKID.OneTapInternalEvents.LOGIN_SUCCESS, (payload) => {
+          const { code, device_id } = payload;
+          VKID.Auth.exchangeCode(code, device_id)
+            .then(console.log)
+            .catch(console.error);
+        });
+      }
+    };
+    document.body.appendChild(script);
+  }, []);
+
   return (
     <div>
       <h1>VK One Tap Login</h1>
-      <script src="https://unpkg.com/@vkid/sdk@3.0.0/dist-sdk/umd/index.js"></script>
-      <script dangerouslySetInnerHTML={{ __html: `
-        if ('VKIDSDK' in window) {
-          const VKID = window.VKIDSDK;
-
-          VKID.Config.init({
-            app: 53969710,
-            redirectUrl: 'https://vercel2-eight-blue.vercel.app/api/vk/callback',
-            responseMode: VKID.ConfigResponseMode.Callback,
-            source: VKID.ConfigSource.LOWCODE,
-            scope: '',
-          });
-
-          const oneTap = new VKID.OneTap();
-
-          oneTap.render({
-            container: document.body,
-            showAlternativeLogin: true,
-            oauthList: ['mail_ru']
-          })
-          .on(VKID.WidgetEvents.ERROR, console.error)
-          .on(VKID.OneTapInternalEvents.LOGIN_SUCCESS, function (payload) {
-            const code = payload.code;
-            const deviceId = payload.device_id;
-            VKID.Auth.exchangeCode(code, deviceId)
-              .then(console.log)
-              .catch(console.error);
-          });
-        }
-      `}} />
+      <div id="vk-button" />
     </div>
   );
 }
