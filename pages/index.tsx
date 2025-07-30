adfd2580db403652fc15cabe88e3fc8ab6cc0e60
1v1ad <1v1ad@proton.me>
2025-07-30 09:38:36 +0300

    Add files via upload

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,25 +1,31 @@
-import Script from "next/script";
+
+import { useEffect } from 'react';
 
 export default function Home() {
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.src = 'https://vk.com/js/api/openapi.js?169';
+    script.async = true;
+    script.onload = () => {
+      // @ts-ignore
+      if (window.VK) {
+        // @ts-ignore
+        window.VK.init({
+          apiId: 53969710,
+          onlyWidgets: true,
+        });
+
+        // @ts-ignore
+        window.VK.Widgets.Auth("vk_auth", {});
+      }
+    };
+    document.body.appendChild(script);
+  }, []);
+
   return (
     <div>
       <h1>VK One Tap Login</h1>
-      <div id="vk_ontap_auth" />
-      <Script
-        strategy="afterInteractive"
-        src="https://vk.com/js/api/openapi.js?169"
-        onLoad={() => {
-          // @ts-ignore
-          if (window.VK) {
-            // @ts-ignore
-            window.VK.Widgets.Auth("vk_ontap_auth", {
-              onAuth: function(userData) {
-                alert("Добро пожаловать, " + userData.first_name);
-              }
-            });
-          }
-        }}
-      />
+      <div id="vk_auth"></div>
     </div>
   );
 }
