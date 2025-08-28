ebda96f0fefd1b93ebeb131efc993e0f58d1f0b3
1v1ad <1v1ad@proton.me>
2025-08-28 03:21:36 +0300

    Delete src directory

--- src/main.tsx
+++
@@ -1,8 +0,0 @@
-// ВАЖНО: прогрев — самым первым импортом
-import './warmup';
-
-import { createRoot } from 'react-dom/client';
-import App from './App.tsx';
-import './index.css';
-
-createRoot(document.getElementById('root')!).render(<App />);
